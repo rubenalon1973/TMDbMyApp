6126bc40ff815c1e5fb56da025e5421e1f53fea6
rubenalon1973 <rubenalonso1973@gmail.com>
2023-08-03 09:29:53 +0200

    Initial Commit

---
+++ TMDbMyApp/TMDbMyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDbMyAppApp.swift
+//  TMDbMyApp
+//
+//  Created by Ruben Alonso on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TMDbMyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
